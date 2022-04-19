aaf583f649c00132da2eb3a84dab6f140e47cf31
Estefan Valencia <evalenci@emich.edu>
2022-04-19 21:33:01 +0000

    feat: redirects you to /movies when starting up the website

--- flaskr/movies.py
+++ flaskr/movies.py
@@ -1,9 +1,13 @@
-from flask import Blueprint, request, render_template
+from flask import Flask, Blueprint, request, render_template, redirect
 from flaskr.db import get_movies
 
-bp = Blueprint('', __name__, url_prefix='/')
+bp = Blueprint('movies', __name__, url_prefix='/')
 
-@bp.route('/', methods=['GET'])
+@bp.route('movies/')
 def movie_list():
     movies = get_movies()
     return render_template('movies.html', movies=movies)
+
+@bp.route('/')
+def sendRedirect():
+    return redirect('movies/')
